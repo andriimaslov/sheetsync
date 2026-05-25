ea2f37a124571d633e829447580c19dd1864bd7d
Andrii Maslov <andrii.dmaslov@outlook.com>
2026-05-25 18:48:05 +0300

    fix: format date

--- app/src/main/java/dev/maslov/sheetsync/ui/components/RuleMetaInfoPanel.kt
+++ app/src/main/java/dev/maslov/sheetsync/ui/components/RuleMetaInfoPanel.kt
@@ -7,13 +7,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import dev.maslov.sheetsync.model.Rule
+import java.time.format.DateTimeFormatter
 
 @Composable
 fun RuleMetaInfoPanel(rule: Rule) {
+    val formatter = DateTimeFormatter.ofPattern("HH:mm dd-MM-yyyy ")
     Column(modifier = Modifier.padding(top = 12.dp)) {
         Text("Sheet name: ${rule.sheetName}")
         Text("Tab name: ${rule.tabName}")
         Text("Last Append Status: ${rule.lastRunStatus}")
-        Text("Last Append At: ${rule.lastRunAt}")
+        Text("Last Append At: ${rule.lastRunAt?.format(formatter)}")
     }
 }
